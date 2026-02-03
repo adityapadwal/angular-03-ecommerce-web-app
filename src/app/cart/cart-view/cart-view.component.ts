fd0ef3327347132b0000fd361b898b33f3ad6af4
Aditya Padwal <adityapadwal02@gmail.com>
2026-02-03 08:40:51 +0530

    feat: Adding cart components

--- src/app/cart/cart-view/cart-view.component.ts
+++ src/app/cart/cart-view/cart-view.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Product } from './../../models/product';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-cart-view',
   templateUrl: './cart-view.component.html',
   styleUrls: ['./cart-view.component.css']
 })
-export class CartViewComponent {
+export class CartViewComponent implements OnInit {
 
+  // Stores all products in cart
+  cartItems: Product[] = [];
+
+  // constructor
+  constructor(private CartService: CartService) {
+
+  }
+
+  // ngOnInit for initialization
+  ngOnInit(): void {
+    this.CartService.getCartItems().subscribe( data => {
+      console.log(data);
+      this.cartItems = data;
+    });
+  }
 }
